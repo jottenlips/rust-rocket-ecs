afce8d5e760798f9ee68378c7aa5e127db152b55
John Ottenlips <johnottenlips@Johns-Mac-mini.attlocal.net>
2022-12-19 13:58:31 -0600

    bump to 3 instances

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ resource "aws_ecs_service" "hello_rocket" {
   cluster         = aws_ecs_cluster.rocket_cluster.id      
   task_definition = aws_ecs_task_definition.rocket_task.arn 
   launch_type     = "FARGATE"
-  desired_count   = 2
+  desired_count   = 3
 
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn 
