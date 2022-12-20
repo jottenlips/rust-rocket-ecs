a7486e28e28b56b9e49c4b08de0c9f4b85a3d361
John Ottenlips <johnottenlips@Johns-Mac-mini.attlocal.net>
2022-12-20 15:07:03 -0600

    revert some networking

--- main.tf
+++ main.tf
@@ -38,28 +38,27 @@ resource "aws_ecs_cluster" "rocket_cluster" {
 }
 
 resource "aws_ecs_task_definition" "rocket_task" {
-  family                   = "rocket-task-def"
+  family                   = "rocket-task"
   container_definitions    = <<DEFINITION
   [
     {
-      "name": "rocket-task-def",
+      "name": "rocket-task",
       "image": "${aws_ecr_repository.rocket_ecr_repo.repository_url}",
       "essential": true,
       "portMappings": [
         {
           "containerPort": 8000,
-          "hostPort": 8000,
-          "protocol": "tcp"
+          "hostPort": 8000
         }
       ],
-      "memory": 1024,
+      "memory": 512,
       "cpu": 256
     }
   ]
   DEFINITION
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  memory                   = 1024
+  memory                   = 512
   cpu                      = 256
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
 }
@@ -97,26 +96,12 @@ resource "aws_alb" "application_load_balancer" {
 }
 
 resource "aws_security_group" "load_balancer_security_group" {
-
-  name        = "load_balancer_security_group"
-  description = "Controls access to the ALB"
-  vpc_id      = aws_default_vpc.default_vpc.id
-
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -132,14 +117,19 @@ resource "aws_lb_target_group" "target_group" {
   target_type = "ip"
   vpc_id      = aws_default_vpc.default_vpc.id
   health_check {
+    healthy_threshold = "2"
+    unhealthy_threshold = "6"
+    interval = "30"
     matcher = "200,301,302"
-    path    = "/"
+    path = "/"
+    protocol = "HTTP"
+    timeout = "5"
   }
 }
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_alb.application_load_balancer.arn
-  port              = 443
+  port              = 80
   protocol          = "HTTP"
   default_action {
     type             = "forward"
@@ -169,22 +159,11 @@ resource "aws_ecs_service" "hello_rocket" {
 
 
 resource "aws_security_group" "service_security_group" {
-  name        = "ecs_security_group"
-  description = "Allows inbound access from the ALB only"
-  vpc_id      = aws_default_vpc.default_vpc.id
-
   ingress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    security_groups = [aws_security_group.load_balancer_security_group.id]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
   }
 
   egress {
